3a6af12371fe84d91bc169dc1cfd29448cb3d5db
rjiuk3566 <emil.hotkowski@gmail.com>
2016-08-20 22:48:02 +0200

    Refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,9 @@
 // Module initialization
 var app = angular.module('pindish', []);
+
+app.directive('navigationBar', function() {
+  return {
+    restrict : 'E',
+    templateUrl : 'navigation.xhtml'
+  };
+});
