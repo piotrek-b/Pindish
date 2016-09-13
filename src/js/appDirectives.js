246d12fced9f00cac2caccfaf221cb884d3aae64
piotrek-b <piotr.bechcicki.o@gmail.com>
2016-09-13 22:24:21 +0200

    Added Sign In/Up Popups with validation.

--- src/js/appDirectives.js
+++ src/js/appDirectives.js
@@ -114,3 +114,24 @@ angular.module('appDirectives', [])
         }
     };
 })
+
+// SIGN IN/UP POPUP DIRECTIVES
+
+.directive("compareTo", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+})
